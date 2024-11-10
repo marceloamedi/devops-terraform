fae29421a55ef23a6e4f1b4046942f6a7fa5862f
marceloamedi <marceloamedi@gmail.com>
2024-11-10 08:23:36 -0300

    fix: lint

--- provider.tf
+++ provider.tf
@@ -1,12 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.75.0"
     }
   }
+  backend "s3" {
+    bucket = "devops-buckets3"
+    key    = "tarraform.tfstate"
+    region = "us-east-1"
+  }
 }
-
 provider "aws" {
-  region= "us-east-1"
+  region = "us-east-1"
 }
