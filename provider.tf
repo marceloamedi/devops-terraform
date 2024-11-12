7d16fb6c58048ddd93ce00825a65b431c5b36bcd
marceloamedi <marceloamedi@gmail.com>
2024-11-12 16:41:51 -0300

    fix: lint terraform fmt

--- provider.tf
+++ provider.tf
@@ -4,6 +4,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.75.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.33.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.16.1"
+    }
   }
   backend "s3" {
     bucket = "devops-buckets3"
@@ -13,4 +22,26 @@ terraform {
 }
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
+  }
 }
